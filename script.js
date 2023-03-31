d07959ef7ce93f4587aeaecfb771539f4260c108
Tr1st0n <tristonxmoulton@hotmail.com>
2023-03-30 20:57:40 -0400

    Working on JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const app = {}
 
 app.apiKey = "352855b1ece3130738315189ae8c3079";
-app.url = "https://api.themoviedb.org/3/genre/movie/list";
+app.url = "https://api.themoviedb.org/3/discover/movie";
 
 const url = new URL(app.url)
 url.search = new URLSearchParams({
@@ -9,7 +9,7 @@ url.search = new URLSearchParams({
     language: 'en-US', 
     sort_by: "popularity.desc",
     year: 2022, 
-    with_genres: "10751",
+    with_genres: "10749", "36", "28", "80",     
     page: 1,
 
 })
@@ -20,5 +20,21 @@ url.search = new URLSearchParams({
         .then(function(jsonResult){
             console.log(jsonResult)
             //Pass our JSON Results to our displayMovies function.
-            app.displayMovies(jsonResult)
+            // app.displayMovies(jsonResult)
         });
+
+// // Define a Method to display the Movies
+// app.displayMovies = (arrayOfFilms) => {
+// // Query the Document and Find the First Ul
+//     const ul = document.querySelector('ul')
+//     // Iterate through the API Data 
+//     arrayOfFilms.filter((data) => {
+//     // Create list Elements 
+//     const listElement = document.createElement('li')
+//     // Create Image Element for Poster
+//     const imageElement = document.createElement('img')
+//     // Add Src and Alt Attributes 
+        
+    
+//      })
+// }
